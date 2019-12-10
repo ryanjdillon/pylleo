d9c4f835b0345ecae28dc63f02f8f3a53126fd92
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-12-10 18:56:39 +0100

    Add version to init

--- src/pylleo/__init__.py
+++ src/pylleo/__init__.py
@@ -1,8 +1,12 @@
 '''Pylleo is collection of utilities for reading and calibrating Little Leonardo
 datalogger data in Python.
-
 '''
 from . import lleoio
 from . import lleocal
 from . import utils
 from . import utils_bokeh
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
