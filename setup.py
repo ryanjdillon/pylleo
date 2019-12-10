38a28defe6f538198c22c8e4fb3d1ae149b322b3
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2019-12-10 18:56:11 +0100

    Update setup.py with src path

--- setup.py
+++ setup.py
@@ -1,33 +1,39 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
+def requirements(fp: str):
+    with open(fp) as f:
+        return [r.strip() for r in f.readlines()]
+
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
+setup_requirements = ["pytest-runner", "setuptools_scm"]
+
 setup(
-    name='pylleo',
-    version='0.4',
-    description=('Utilities for working with Little Leonardo datalogger '
-          'data in Python'),
+    name="pylleo",
+    description=("Utilities for working with Little Leonardo datalogger "
+          "data in Python"),
     long_description=long_description,
-    author='Ryan J. Dillon',
-    author_email='ryanjamesdillon@gmail.com',
-    url='https://github.com/ryanjdillon/pylleo',
-    download_url='https://github.com/ryanjdillon/pylleo/archive/0.4.tar.gz',
-    license='GPL-3.0+',
-    packages=['pylleo'],
-    install_requires=[
-        'bokeh==0.12.6',
-        'click==6.7',
-        'pandas==0.20.3',
-        'yamlord==0.4'],
-    scripts=['bin/pylleo-cal'],
+    author="Ryan J. Dillon",
+    author_email="ryanjamesdillon@gmail.com",
+    url="https://github.com/ryanjdillon/pylleo",
+    license="GPL-3.0+",
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    install_requires=requirements("requirements.in"),
+    tests_requires=requirements("requirements_test.txt"),
+    scripts=["bin/pylleo-cal"],
     include_package_data=True,
-    keywords=['datalogger','accelerometer','biotelemetry'],
+    use_scm_version={
+        "write_to": "src/pylleo/_version.py",
+        "relative_to": __file__,
+    },
+    keywords=["datalogger","accelerometer","biotelemetry"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.5'],
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.5"],
     zip_safe=False,
     )
